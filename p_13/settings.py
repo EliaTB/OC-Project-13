a78f1895f3f6d642a040622041221a69e72377ad
Elia <beneli.t@icloud.com>
2019-03-25 13:34:27 +0100

    created password reset functionality

--- p_13/settings.py
+++ p_13/settings.py
@@ -129,3 +129,14 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'resv:home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+USER = os.environ.get('EMAIL_USER')
+PASSW = os.environ.get('EMAIL_PASS')
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = USER
+EMAIL_HOST_PASSWORD = PASSW
